534cb6d6b38efd69a21c5eba570a92f938105ef2
kangsuroro <kangsuroro2907@gmail.com>
2021-02-14 13:19:47 +0900

    add axis y

--- src/App.tsx
+++ src/App.tsx
@@ -68,11 +68,32 @@ function App() {
     svg.append('g').call(x_axis);
   }, []);
 
+  useEffect(() => {
+    var width = 400, height = 400;
+
+    var data = [10, 15, 20, 25, 30];
+    var svg = window.d3.select("body")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height);
+
+    var scale = window.d3.scaleLinear()
+                  .domain([window.d3.min(data), window.d3.max(data)])
+                  .range([height/2, 0]);
+
+    var y_axis = window.d3.axisLeft()
+                  .scale(scale);
+
+    svg.append("g")
+       .attr("transform", "translate(50, 10)")
+       .call(y_axis);
+  }, [])
+
   return (
     <div className="App">
+      <div className="axis-y"></div>
       <div className="bar-chart"></div>
       <div className="axis-x"></div>
-      <div className="axis-y"></div>
     </div>
   );
 }
